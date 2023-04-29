5fc50d33853369030888a4e7a9927361d7d63210
guirib00 <guilhermerib2013@hotmail.com>
2023-04-29 01:46:29 +0000

    Adicionei os botoes e o calculo

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+	const [peso, setPeso] = useState(0);
+	const [altura, setAltura] = useState(0);
+	const [resultado, setResultado] = useState(0);
+	
+	function Calcular (){
+		let r = parseFloat(peso) / (parseFloat(altura)*parseFloat(altura));
+		setResultado(r);
+	}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+		  <View style={styles.titulo}>
+		  	<Text style={styles.textoTitulo}>CALCULADORA IMC</Text>
+		  </View>
+		  
+		  <View style={styles.blocoImagem}>
+		  	<Image style={styles.imagem}
+        		source={
+					require('./assets/unnamed.png')
+				}
+      		/>
+		  </View>
+		  
+		  <View style={styles.bloco}>
+		  	<Text style={styles.label}> Digite seu peso: </Text>
+			  <TextInput 
+				  style={styles.input}
+				  value={peso}
+				  onChangeText={(peso)=>setPeso(peso)}
+			  />
+			  <Text style={styles.label}> Digite sua altura: </Text>
+			  <TextInput 
+				  style={styles.input}
+				  value={altura}
+				  onChangeText={(altura)=>setAltura(altura)}
+			  />
+		  </View>
+		  
+		  <View style={styles.botResul}>
+			  <TouchableOpacity style={styles.botao} onPress={Calcular}>
+				  <Text style={styles.txtBotao}>Calcular</Text>
+			  </TouchableOpacity>
+			  <Text style={styles.label}> Resultado: {resultado}</Text>
+		  </View>
+	  </View>
+		  
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	backgroundColor:'#fff',
+	},
+	titulo: {
+		backgroundColor:'#00adc2',
+		height:'10%',
+		justifyContent:'center'
+	},
+	textoTitulo:{
+		fontSize:20,
+		textAlign:'center',
+		color:'#fff'
+	},
+	imagem:{
+		width:180,
+		height:180,
+},
+	blocoImagem:{
+	alignItems: 'center',
+	marginTop:30
+	},
+	input:{
+	borderWidth:2,
+	fontSize:20,
+	width:50,
+	borderRadius:10,
+},
+	bloco:{
+		alignItems:'center',
+		marginTop:30,	
+		flexDirection:'row',
+		margin:'auto',
+	},
+	botao:{
+	backgroundColor:'#00adc2',
+	alignItems:'center',
+	width:150,
+	borderRadius:10,
+	marginTop:10,
   },
+  txtBotao:{
+	color:"#FFF",
+	fontSize:30
+  },
+	botResul:{
+		alignItems:'center',
+		margin:'auto',
+	}
 });
